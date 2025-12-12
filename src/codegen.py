21443beb9a91ec226537f62b7b90e4caad07d18e
Monir Hussain <monir22205101801@diu.edu.bd>
2025-12-12 11:48:54 +0600

    Update codegen.py

--- src/codegen.py
+++ src/codegen.py
@@ -1,9 +1,5 @@
-# src/codegen.py
 from ast_n import Number, Var, BinOp, Assign, Print
 from typing import List, Tuple
-
-# Instruction tuples: ("PUSH", value), ("LOAD", name), ("STORE", name),
-# ("ADD",), ("SUB",), ("MUL",), ("DIV",), ("PRINT",)
 
 class CodeGen:
     def __init__(self):
